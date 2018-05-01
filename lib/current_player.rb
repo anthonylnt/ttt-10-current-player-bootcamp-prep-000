553c0d36ab1a2efe118ddc7eb8d70fb1694c39ed
Anthony Elijah <lionnet.ant@gmail.com>
2018-05-01 19:19:37 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,9 +5,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
-    return "X"
-  elsif turn_count(board) % 2 == 1 
-    return "0"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "0"
 end
